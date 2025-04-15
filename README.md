3ee434eb3fd4dd718bb3a2d1ac8a3abddbeac969
zyhyuheng <zyhyuheng@163.com>
2025-04-15 08:31:10 +0800

    初始版本提交

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # markdown-app
-基于Cherry Markdown创建的桌面应用程序
+## 依赖
+1. 基于[Cherry Markdown](https://github.com/Tencent/cherry-markdown)创建的桌面应用程序
+2. 桌面应用依赖于[electron](https://github.com/electron/electron)
+3. 打包依赖于[Electron Forge](https://www.electronforge.io/)，[简单教程](https://www.electronjs.org/zh/docs/latest/tutorial/%E6%89%93%E5%8C%85%E6%95%99%E7%A8%8B)
+## 开始
+:::warning
+安装最新版本node.js
+:::
+
+1. 安装依赖
+``` 
+npm install
+```
+2. 启动
+``` 
+npm run start
+```
+3. 打包成windows程序
+``` 
+npm run make
+```
+打包后程序路径
+	- markdown-app\out\zyh-markdown-editor-win32-x64为可直接运行程序
+	- markdown-app\out\make\squirrel.windows\x64下的exe为安装程序
+
+
---
+++ main.js
@@ -0,0 +1,145 @@
+const { app, BrowserWindow, Menu, ipcMain, dialog } = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+// run this as early in the main process as possible
+if (require('electron-squirrel-startup')) app.quit();
+let currentFilePath = null;
+
+
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: false
+    }
+  });
+
+  // 添加快捷键事件监听器
+  mainWindow.webContents.on('before-input-event', (event, input) => {
+    if (input.control && input.key.toLowerCase() === 's') {
+      event.preventDefault();
+      if (currentFilePath) {
+        mainWindow.webContents.send('save-markdown');
+      } else {
+        dialog.showSaveDialog({
+          filters: [{ name: 'Markdown', extensions: ['md'] }]
+        }).then(({ filePath }) => {
+          if (filePath) {
+            currentFilePath = filePath;
+            mainWindow.webContents.send('save-markdown');
+          }
+        });
+      }
+    }
+  });
+
+  // 读取markdown文件内容
+  const markdownPath = path.join(__dirname, 'markdown', 'basic.md');
+  try {
+    const content = fs.readFileSync(markdownPath, 'utf-8');
+    mainWindow.webContents.on('did-finish-load', () => {
+      mainWindow.webContents.send('markdown-content', content);
+    });
+  } catch (error) {
+    console.error('Error reading markdown file:', error);
+  }
+
+  mainWindow.loadFile('./index.html');
+
+  const template = [
+    {
+      label: '文件',
+      submenu: [
+        {
+          label: '打开',
+          click: async () => {
+            const { filePaths } = await dialog.showOpenDialog({
+              properties: ['openFile'],
+              filters: [{ name: 'Markdown', extensions: ['md'] }]
+            });
+            if (filePaths.length > 0) {
+              currentFilePath = filePaths[0];
+              const content = fs.readFileSync(currentFilePath, 'utf-8');
+              mainWindow.webContents.send('markdown-content', content);
+            }
+          }
+        },
+        {
+          label: '保存',
+          click: async () => {
+            if (currentFilePath) {
+              mainWindow.webContents.send('save-markdown');
+            } else {
+              const { filePath } = await dialog.showSaveDialog({
+                filters: [{ name: 'Markdown', extensions: ['md'] }]
+              });
+              if (filePath) {
+                currentFilePath = filePath;
+                mainWindow.webContents.send('save-markdown');
+              }
+            }
+          }
+        },
+        {
+          label: '另存为',
+          click: async () => {
+            const { filePath } = await dialog.showSaveDialog({
+              filters: [{ name: 'Markdown', extensions: ['md'] }]
+            });
+            if (filePath) {
+              currentFilePath = filePath;
+              mainWindow.webContents.send('save-markdown');
+            }
+          }
+        },
+        { type: 'separator' },
+        { role: 'quit', label: '退出' }
+      ]
+    },
+    {
+      label: '编辑',
+      submenu: [
+        { role: 'undo', label: '撤销' },
+        { role: 'redo', label: '重做' },
+        { type: 'separator' },
+        { role: 'cut', label: '剪切' },
+        { role: 'copy', label: '复制' },
+        { role: 'paste', label: '粘贴' },
+        { role: 'selectAll', label: '全选' }
+      ]
+    },
+    {
+      label: '视图',
+      submenu: [
+        { role: 'reload', label: '重新加载' },
+        { role: 'toggleDevTools', label: '开发者工具' },
+        { type: 'separator' },
+        { role: 'resetZoom', label: '重置缩放' },
+        { role: 'zoomIn', label: '放大' },
+        { role: 'zoomOut', label: '缩小' },
+        { type: 'separator' },
+        { role: 'togglefullscreen', label: '全屏' }
+      ]
+    }
+  ];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
